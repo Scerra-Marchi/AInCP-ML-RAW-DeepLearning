23501b015cfe9a379675e3100ce61c5e99bf3747
Davide Marchi <davidemarchi00@gmail.com>
2026-02-23 14:59:25 +0100

    fixed regressor dumping

--- Pipeline/skorch_models.py
+++ Pipeline/skorch_models.py
@@ -117,6 +117,7 @@ def make_gru_regressor_net():
                     threshold_mode="rel",
                     lower_is_better=True,
                     load_best=True,
+                    sink=None
                 ),
             ),
         ],
